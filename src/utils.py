df2435d00a682bc744571388931e93d6e4696b10
ovtyne <133908160+ovtyne@users.noreply.github.com>
2023-08-30 13:48:27 +0400

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import json
 
 def read_file(filename):
     result_list = []
-    with open(filename, 'r') as file:
+    with open(filename, 'r', encoding="utf-8") as file:
         data = json.load(file)
     for item in data:
         result_list.append((item['id'], item['name']))
